fe8d3043d91471f4107116b30e10f6d54331bd30
boqipod52@gmail.com <fuku1026>
2017-12-02 10:25:44 +0900

    modified to check stringified results

--- embed-python/vs-project/pythonnet-test-cs/Program.cs
+++ embed-python/vs-project/pythonnet-test-cs/Program.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Python.Runtime;
 
 namespace pythonnet_test_cs
 {
     class Program
     {
-        static void Main(string[] args)
+        static void test1()
         {
             using (Py.GIL())
             {
+                // PythonEngine.Initialize();
                 dynamic np = Py.Import("numpy");
                 Console.WriteLine(np.cos(np.pi * 2));
 
@@ -24,13 +22,20 @@ namespace pythonnet_test_cs
 
                 dynamic a = np.array(new List<float> { 1, 2, 3 });
                 Console.WriteLine(a.dtype);
+                Console.WriteLine(a);
 
                 // dynamic b = np.array(new List<float> { 6, 5, 4 }, dtype: np.int32);
                 dynamic b = np.array(new List<float> { 6, 5, 4 });
                 Console.WriteLine(b.dtype);
+                Console.WriteLine(b);
 
                 Console.WriteLine(a * b);
             }
+        }
+
+        static void Main(string[] args)
+        {
+            test1();
         }
     }
 }
